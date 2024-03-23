b4dfde91849ca5aef8acafb241ea94f441567436
Taea Vogel <taelxvie@gmail.com>
2024-03-23 18:16:50 -0400

    fix mobile spacing

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
   return (
     <div className="justify-top flex min-h-screen flex-col items-center gap-4">
       <h1 className="pt-12 text-5xl font-bold text-[#6a7edc]">Re:View</h1>
-      <h2 className="text-xl font-light">
+      <h2 className="text-center text-xl font-light">
         What the people are saying about the top apps on iOS
       </h2>
       <AppSelect onAppSelect={handleAppSelection} />
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -4,12 +4,14 @@ import { ReviewItem } from "../types/Reviews";
 const ReviewCard: React.FC<{ review: ReviewItem }> = ({ review }) => {
   return (
     <div className="rounded-lg border-2 p-4 shadow-sm hover:shadow-md">
-      <div className="justify-left flex flex-row items-center gap-2">
-        <div className="flex-shrink-0 text-xl font-bold text-gray-800">
+      <div className="justify-left flex flex-row flex-wrap items-center gap-2">
+        <div className="flex-shrink-0 text-lg font-bold text-gray-800 md:text-2xl">
           {review.Author}
         </div>
-        <div className="flex-grow">{"⭐️".repeat(review.Score)}</div>
-        <div className="flex-grow text-right text-sm font-light">
+        <div className="flex-grow text-sm md:text-lg">
+          {"⭐️".repeat(review.Score)}
+        </div>
+        <div className="flex flex-shrink-0 text-left text-sm font-light">
           {new Date(review.Date).toLocaleString("en-US", {
             month: "short",
             day: "numeric",
